0004bc905038f3e6205f8ae3a315244b1e3f674b
jmaliszewski <j.maliszewski1998@gmail.com>
2019-03-11 12:50:47 +0100

    Pierwsza wersja aplikacji

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+      /*  public ActionResult Index()
+        {
+            return View();
+        }*/
+
+       public ActionResult FizzBuzz()
+        {
+            Class liczba = new Class();
+
+            return View(liczba);
+        }
+
+        [HttpPost]
+        public ActionResult FizzBuzz(Class number)
+        {
+
+            if (number.liczba < 1 || number.liczba > 100)
+            {
+                number.odpowiedz = "Dozwolone są tylko liczby całkowite w przedziale od 1 do 100";
+                return View("Odpowiedz", number);
+            }
+
+            if (number.liczba % 5 == 0 && number.liczba % 3 == 0)
+            {
+                number.odpowiedz = "FizzBuzz";
+                return View("Odpowiedz", number);
+            }
+
+            if (number.liczba % 3 == 0)
+            {
+                number.odpowiedz = "Fizz";
+                return View("Odpowiedz", number);
+            }
+
+            if (number.liczba % 5 == 0)
+            {
+                number.odpowiedz = "Buzz";
+                return View("Odpowiedz", number);
+            }
+
+            else
+            {
+                number.odpowiedz = number.liczba.ToString();
+                return View("Odpowiedz", number);
+            }
+        }
+    }
+}
---
+++ WebApplication2/Models/Class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Class
+    {
+        [Display(Name = "Podaj liczbe" )]
+
+        public int liczba { get; set; }
+
+        public string odpowiedz;
+    }
+}
